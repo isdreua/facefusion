f23a8c1ffd595880fc072d7e4699fb0df9cfdff8
MaxHelios <139435744+MaxHelios@users.noreply.github.com>
2026-08-06 20:54:38 +0200

    change udp stream options

--- facefusion/streamer.py
+++ facefusion/streamer.py
@@ -117,6 +117,13 @@ def open_stream(stream_mode : StreamMode, stream_resolution : str, stream_fps : 
 		commands.extend(ffmpeg_builder.set_input('-'))
 		commands.extend(ffmpeg_builder.set_stream_mode('udp'))
 		commands.extend(ffmpeg_builder.set_stream_quality(2000))
+		commands.extend([
+			'-c:v', 'libx264',
+			'-preset', 'ultrafast',
+			'-tune', 'zerolatency',
+			'-g', '30',
+			'-pix_fmt', 'yuv420p'
+		])
 		commands.extend(ffmpeg_builder.set_output('udp://localhost:27000?pkt_size=1316'))
 
 	if stream_mode == 'v4l2':
